dce41a039e4c8735c02c05fcf1d62d65ed8aa15c
Felipe Honorato <felipesousa@eng.ci.ufpb.br>
2021-02-17 07:14:53 -0300

    implementing request hooks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, g
 
 app = Flask(__name__)
 
@@ -10,4 +10,17 @@ def index():
 
 @app.route('/user/<name>')
 def identifier(name):
-    return f'<h1> Hello {name}!'
+    ip_address = request.remote_addr
+    endpoint = request.endpoint
+    return f'Hi {name}, your IP adress is {ip_address} and you\' re at {endpoint}'
+
+
+@app.route('target')
+def target():
+    return g.target +'\n'
+
+
+# Request hooks 
+@app.before_request
+def before_r():
+    g.target = request.args.get('target'.'default')
