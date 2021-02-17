cda07b67d3c053cbc08bfdee48278e53946c69ae
Felipe Honorato <felipesousa@eng.ci.ufpb.br>
2021-02-17 07:21:17 -0300

    Responses

--- app.py
+++ app.py
@@ -20,7 +20,17 @@ def target():
     return g.target +'\n'
 
 
-# Request hooks 
+# Request hooks: Useful for storing users and initializing database connections. 
 @app.before_request
 def before_r():
     g.target = request.args.get('target'.'default')
+    g.user = request.remote_addr
+
+# Response -> status code sent through invoked function.
+
+@app.route('/ip_address/<>')
+def verifying_same_ip():
+    if g.user == '127.0.0.2':
+        return '02 adress', 200 # 200 Code is by default set as a positive sign for the function invoked
+    else:
+        return request.remote_addr, 400
